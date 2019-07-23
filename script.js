aebf0d4137fb4a6dc3fe2e7e509036fef5f166da
Maham Siddiqui <49103875+MahamSiddiqui@users.noreply.github.com>
2019-07-23 15:37:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let randomNumber;
+let count = 0;
+function resetGame() {
+    randomNumber = parseInt(Math.random() * 101);
+    count = 0;
+}
+resetGame();
+console.log(randomNumber);
+
+function guessTheNumber() {
+    let userInput = parseInt(document.querySelector("input").value);
+
+    if (userInput > randomNumber) {
+        alert("Too Big!");
+        count++;
+
+    } else if (userInput < randomNumber) {
+        alert("Too Small!");
+        count++;
+
+    } else {
+        alert("Congratulations! You have won! Number of guess you attemp " + count);
+        resetGame();
+        console.log(randomNumber);
+
+    }
+}
